ead26c6f8c32949a9687e8558c3c6be7bc0afda8
Matthew Moore <mmoore@allied360.com>
2015-09-10 08:32:30 -0500

    Data Massaging

--- src/response/WhenGraph.php
+++ src/response/WhenGraph.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Matthew
- * Date: 9/9/2015
- * Time: 7:14 PM
- */
-
 namespace F2klabs\Digimind\response;
-
 
 use Carbon\Carbon;
 
@@ -27,7 +19,40 @@ class WhenGraph extends Response{
             array_push($result, [$time->timestamp, $dataPoint->count]);
         }
 
-        return array_reverse($result);
+        $graphData = array_reverse($result);
+        $stats = $this->getChartStats($graphData);
+
+        return ['stats'=>$stats, "graphData"=>$graphData];
+    }
+
+    protected function getChartStats(Array $data)
+    {
+        $minimum = PHP_INT_MAX;
+        $maximum = 0;
+        $average = 0;
+        $total = 0;
+
+        $i =0;
+        foreach($data as $dataPoint)
+        {
+            $total += $dataPoint[1];
+
+            if($dataPoint[1] > $maximum)
+                $maximum = $dataPoint[1];
+
+            if($minimum < $dataPoint[1])
+            {
+                $minimum = $dataPoint[1];
+            }
+
+            //Iterate for Average
+            $i++;
+        }
+
+        $average = round(($total/$i));
+
+        return ["total"=>$total, "average"=>$average, "minimum"=>$minimum, "maximum"=>$maximum];
+
     }
 
 }
